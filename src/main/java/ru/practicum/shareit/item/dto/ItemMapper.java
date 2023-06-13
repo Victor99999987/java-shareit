8dfdf3f4f3f0030b0cd6e86353913fa2cca652e6
Victor99999987 <volozhaninvs@xn--90acjidgfhdqiaistf2e9i.xn--p1ai>
2023-06-13 22:58:48 +0700

    fix: добавлены приватные конструкторы в утилитные классы (мапперы)

--- src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
@@ -2,8 +2,10 @@ package ru.practicum.shareit.item.dto;
 
 import ru.practicum.shareit.item.model.Item;
 
+public final class ItemMapper {
+    private ItemMapper() {
+    }
 
-public class ItemMapper {
     public static ItemDto toDto(Item item) {
         return ItemDto.builder()
                 .id(item.getId())
--- src/main/java/ru/practicum/shareit/user/dto/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserMapper.java
@@ -2,7 +2,10 @@ package ru.practicum.shareit.user.dto;
 
 import ru.practicum.shareit.user.model.User;
 
-public class UserMapper {
+public final class UserMapper {
+    private UserMapper() {
+    }
+
     public static UserDto toDto(User user) {
         return UserDto.builder()
                 .id(user.getId())
